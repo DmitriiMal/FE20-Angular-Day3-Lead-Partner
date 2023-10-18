fa214918d61400e2b94a0004f85c19279913495d
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-18 11:53:54 +0200

    add products

---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const petProducts = [
+  {
+    id: 1,
+    name: 'Premium Dog Food',
+    description:
+      'High-quality dog food with essential nutrients for a healthy dog.',
+    price: 35.99,
+    img: 'https://images.unsplash.com/photo-1601758228006-964e41e5e8eb?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fERvZyUyMEZvb2R8ZW58MHx8MHx8fDA%3D&w=800',
+  },
+  {
+    id: 2,
+    name: 'Interactive Cat Toy',
+    description:
+      'Keep your cat entertained for hours with this interactive toy.',
+    price: 19.99,
+    img: 'https://images.unsplash.com/photo-1616493039661-8a63de6ead09?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Q2F0JTIwVG95fGVufDB8fDB8fHww&w=800',
+  },
+  {
+    id: 3,
+    name: 'Small Animal Bedding',
+    description:
+      'Soft and comfortable bedding for small pets like hamsters and rabbits.',
+    price: 12.49,
+    img: 'https://cdn.pixabay.com/photo/2016/03/20/20/51/gerbil-1269468_1280.jpg',
+  },
+  {
+    id: 4,
+    name: 'Fish Tank Filter',
+    description:
+      'Efficient filter system to maintain a clean and healthy aquarium environment.',
+    price: 29.99,
+    img: 'https://m.media-amazon.com/images/I/81GG2t-HP0L._AC_SL1500_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Bird Cage',
+    description:
+      "Spacious and durable bird cage for your feathered friend's comfort.",
+    price: 49.95,
+    img: 'https://images.unsplash.com/photo-1552826580-0d47cf898dee?auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8QmlyZCUyMENhZ2V8ZW58MHx8MHx8fDA%3D&w=800',
+  },
+  {
+    id: 6,
+    name: 'Pet Grooming Kit',
+    description:
+      'Complete grooming set for pets, including brushes, clippers, and nail trimmers.',
+    price: 44.79,
+    img: 'https://www.qcpetstudies.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdog-grooming-kit.224599e7.jpg&w=1920&q=75',
+  },
+];
